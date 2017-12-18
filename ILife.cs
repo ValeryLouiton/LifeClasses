250e82c1ee518342882389da0c3f66b4cdcebbbd
Technifutur <600@technifutur.be>
2017-12-18 12:22:42 +0100

    Création d\'une interface

---
+++ ILife.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILife
+{
+    void m_Die();
+    void m_Resurrect();
+    void m_GainHealthPoints(float _howMuch);
+    void m_LoseHealthPoints(float _howMuch);
+}
+
+//------------------------------------------------------
+
+public abstract class LifeAdapter : MonoBehaviour, ILife
+{
+    public abstract void m_Die();
+    public abstract void m_Resurrect();
+    public abstract void m_GainHealthPoints(float _howMuch);
+    public abstract void m_LoseHealthPoints(float _howMuch);
+}
